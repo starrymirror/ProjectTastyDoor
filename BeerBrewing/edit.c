c145a7e10136ae027015d7b503a3b76efe44922f
vegard egeland <vegare15@student.uia.no>
2018-12-04 20:00:47 +0100

    project added

---
+++ BeerBrewing/edit.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//
+// Created by vegar on 04.12.2018.
+//
+
+void edit(){
+    printf("two\n");
+}
+
---
+++ BeerBrewing/recipe.c
@@ -0,0 +1,11 @@
+//
+// Created by vegar on 04.12.2018.
+//
+
+#include <stdio.h>
+
+void recipe(){
+    printf("one\n");
+}
+
+
